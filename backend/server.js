aac35a39ca0c7fb5160c1172edcef618a94ead05
brianchilds-22 <brianchilds22@msn.com>
2021-04-11 12:19:29 -0600

    Image

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
@@ -33,6 +35,8 @@ res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
 const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 app.use('/products', express.static(path.join(__dirname, '/products')))
 
 if (process.env.NODE_ENV === 'production') {
